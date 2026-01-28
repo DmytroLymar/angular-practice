e6917625d8d091a07386f0f0d821ebabdd8771a9
Dmytro Lymar <dmytrolymar901@gmail.com>
2026-01-28 11:56:20 +0200

    rxjs

--- src/components/counter/counter.component.ts
+++ src/components/counter/counter.component.ts
@@ -2,19 +2,18 @@ import { Component, inject } from '@angular/core';
 import { CounterControlsComponent } from './counter-controls.component';
 import { CounterDisplayComponent } from './counter-display.component';
 import { CounterService } from './counter.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
   templateUrl: './counter.component.html',
-  imports: [CounterControlsComponent, CounterDisplayComponent],
+  imports: [CounterControlsComponent, CounterDisplayComponent, AsyncPipe],
 })
 export class CounterComponent {
   private readonly counterService = inject(CounterService);
 
-  get count(): number {
-    return this.counterService.count;
-  }
+  readonly count$ = this.counterService.count$;
 
   increase(): void {
     this.counterService.increase();
--- src/components/counter/counter.service.ts
+++ src/components/counter/counter.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class CounterService {
-  count = 0;
+  private readonly _count$ = new BehaviorSubject<number>(0);
+
+  readonly count$ = this._count$.asObservable();
 
   increase(): void {
-    this.count++;
+    this._count$.next(this._count$.value + 1);
   }
   decrease(): void {
-    if (this.count > 0) {
-      this.count--;
-    }
+    this._count$.next(Math.max(0, this._count$.value - 1));
   }
   reset(): void {
-    this.count = 0;
+    this._count$.next(0);
   }
 }
